0533b8cc314b674604ebecf02777eac2f435c7bd
Corn887 <162303781+Corn887@users.noreply.github.com>
2024-04-05 19:23:41 +0800

    Update BMI計算2.py
    使排版更整潔

--- BMI計算2.py
+++ BMI計算2.py
@@ -3,10 +3,12 @@ height_cm_input = input("你的身高是(cm)：")
 height_cm = float(height_cm_input)
 weight_kg_input = input("你的體重是(kg)：")
 weight_kg = float(weight_kg_input)
+
 # BMI的運算過程 + 簡化數字
 height_m = height_cm/100
 BMI_initial = weight_kg/pow(height_m,2)
 BMI = round(BMI_initial,2)   # 四捨五入至小數點後第二位
+
 # 分析BMI的意義
 show = "你的BMI為：" + str(BMI) + " 屬於"
 if 35 <= BMI:
