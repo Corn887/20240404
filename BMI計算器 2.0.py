57a00cdbc31e2cb85b432926d084bb4b1527c766
Corn887 <162303781+Corn887@users.noreply.github.com>
2024-05-02 11:10:44 +0800

    Update BMI計算器 2.0.py

--- BMI計算器 2.0.py
+++ BMI計算器 2.0.py
@@ -5,7 +5,7 @@ weight_kg = float(input("你的體重是(kg)："))
 
 # BMI的運算過程 + 簡化數字
 height_m = height_cm/100
-BMI_initial = weight_kg/pow(height_m,2)
+BMI_initial = weight_kg/pow(height_m,2)   # pow用來取平方
 BMI = round(BMI_initial,2)   # 四捨五入至小數點後第二位
 
 # 分析BMI的意義
