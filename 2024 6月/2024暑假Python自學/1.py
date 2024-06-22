9059e36d35717a12d06be76a7964c915db630996
Corn887 <162303781+Corn887@users.noreply.github.com>
2024-06-22 16:19:28 +0800

    Add files via upload

---
+++ 2024 6月/2024暑假Python自學/1.py
@@ -0,0 +1,19 @@
+# for 迴圈
+for letter in "西片你好":
+    print(letter)
+
+for num_1 in [0, 1, 2, 3, 4]:
+    print(num_1)
+
+for num_2 in range(5):  # range(5)相當於[0,1,2,3,4]
+    print(num_2)
+
+print("你好")
+
+my_list_1 = ['item1', 'item2', 'item3',
+             'item4', 'item5', 'item6']
+
+print(my_list_1)
+
+print("早安\
+你好喔")
